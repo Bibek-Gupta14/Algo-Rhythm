c9e7fed6fd8dc6c29d7e214ffca7f7139b505d6c
Bibek Gupta <biplabgupta50@gmail.com>
2026-09-15 19:44:51 +0530

    Time: 23 ms (84.37%), Space: 65.7 MB (25.59%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.java
+++ 0525-contiguous-array/0525-contiguous-array.java
@@ -6,13 +6,7 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, -1);
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 1) {
-                sum += 1;
-            }
-            if (nums[i] == 0) {
-                sum -= 1;
-            }
-
+            sum += (nums[i] == 1)? 1: -1;
             if (map.containsKey(sum)) {
                 max = Math.max(max, i - map.get(sum));      // stores ('current index' - 'last index when same scenario')
             } else {
