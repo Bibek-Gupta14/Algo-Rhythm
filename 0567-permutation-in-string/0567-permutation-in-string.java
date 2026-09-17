9f135fbd77d6496e5c6dd38895bd37d558e86269
Bibek Gupta <biplabgupta50@gmail.com>
2026-09-17 19:35:12 +0530

    Time: 9 ms (38.24%), Space: 44.3 MB (37.87%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int s1_freq[] = new int[26];  // only lowercase
+        int s2_freq[] = new int[26];  
+
+        if(s1.length() > s2.length()) return false;
+
+        // initialise the window 
+        for(int i=0; i<s1.length(); i++) {          // s1 = ab, -> thats why s1.length
+            s1_freq[s1.charAt(i) - 'a']++;
+            s2_freq[s2.charAt(i) - 'a']++;
+        }
+
+        if(is_Matched(s1_freq, s2_freq)) {          // what if s1 = 'ab' and s2 = 'bac' -> first one only
+            return true;
+        }
+
+        for(int i=1; i<s2.length() - s1.length()+1; i++) {  
+
+            //slide the window
+            s2_freq[s2.charAt(i-1) - 'a']--;   // remove the 1st element of window
+            s2_freq[s2.charAt(i + s1.length() - 1) - 'a']++;   // add the new element to window
+            
+            if(is_Matched(s1_freq, s2_freq)) {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+
+    private boolean is_Matched(int s1_freq[], int s2_freq[]) {
+        for(int i=0 ;i<26; i++) {
+            if(s1_freq[i] != s2_freq[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
